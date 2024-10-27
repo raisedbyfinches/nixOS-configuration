a2cf3e252374e9f731f630860a8d8872e793764a
karetsu <20670199+karetsu@users.noreply.github.com>
2024-10-27 19:36:44 +0000

    20241027: update flake lock

--- home.nix
+++ home.nix
@@ -80,24 +80,24 @@
     size = 16;
   };
 
-  gtk = {
-    enable = true;
+#  gtk = {
+#    enable = true;
 
 #    theme = {
 #      package = pkgs.flat-remix-gtk;
 #      name = "Flat-Remix-GTK-Grey-Darkest";
 #    };
 
-    iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
-    };
+#    iconTheme = {
+#      package = pkgs.gnome.adwaita-icon-theme;
+#      name = "Adwaita";
+#    };
 
 #    font = {
 #      name = "Sans";
 #      size = 11;
 #    };
-  };
+#  };
 
 
 
