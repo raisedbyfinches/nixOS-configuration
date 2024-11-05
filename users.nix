01bec9e51a8d771c1897839e56ea438a77dcd22d
karetsu <20670199+karetsu@users.noreply.github.com>
2024-11-05 16:45:13 +0000

    20241105: update flake lock

--- users.nix
+++ users.nix
@@ -33,6 +33,7 @@
           wl-clipboard-rs
           swappy
           ydotool
+          easyeffects
 
           # haskell packages
           haskellPackages.stylish-haskell
@@ -53,7 +54,7 @@
           # media
           blanket
           
-	  # office
+          # office
           typst
           typst-fmt
           typst-live
