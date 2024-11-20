6a2e19daf491d8fd4009b487ad89897a0319d761
karetsu <20670199+karetsu@users.noreply.github.com>
2024-11-20 09:45:17 +0000

    20241120: update flake lock

--- home.nix
+++ home.nix
@@ -120,7 +120,7 @@ in
           width = 350;
           height = 160;
           origin = "bottom-right";
-          offset = "0x0";
+          offset = "0x20";
           transparency = 0;
           idle_threshold = 120;
           sticky_history = "yes";
